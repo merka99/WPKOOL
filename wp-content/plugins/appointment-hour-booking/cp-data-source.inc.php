10fd9fdca05d36ff6244ddc85524cec3632b4fbf
merlepakkoiktkhk <merle.pakko@khk.ee>
2019-01-14 16:27:52 +0200

    deleted plugin `Appointment Hour Booking` version 1.0.77

--- wp-content/plugins/appointment-hour-booking/cp-data-source.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-  // for datasource fields go to https://apphourbooking.dwbooster.com/download
-	
-?>
--- wp-content/plugins/appointment-hour-booking/cp-widget.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-  // to get the widget go to https://apphourbooking.dwbooster.com/download
-  
-?>
